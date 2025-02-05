ab57ea88bb324a2fa9a1255a30c29f39e16c6b34
matias <matiasbrighent5@gmail.com>
2025-02-04 23:09:36 -0300

    adicionando "conclusao de tarefa"

--- main.js
+++ main.js
@@ -4,17 +4,24 @@ $(document).ready(function(){
 
         const novaTarefa = $('#tarefa').val();
         console.log(novaTarefa);
-        const novoItem =$('<li></li>');
+        const novoItem =$('<li style="display: none"></li>');
         $(`<li> ${novaTarefa}</li>`).appendTo(novoItem);
-        $(`
-            <div class="lista">
-                <ul>
-                    <li>${novaTarefa}</li>
-                </ul>
-            </div>
-            `)
-        $(novoItem).appendTo('ul')
-        $('#tarefa').val('')
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(700);
+        $('#tarefa').val('');
+        
+        $('ul').on('click','li', function(){
+            $(this).css('text-decoration', 'line-through');
+            $(this).css('color', 'red');
+        })
 
+        $('ul').on('dblclick','li', function(){
+            $(this).css('text-decoration', 'none');
+                $(this).css('color', 'black')
+        })
+
+        $('#limpar').on('click', function(){
+            $('li').remove();
+        })
     })
 })
