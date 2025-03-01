f6f0767e93290eeeca5348e37a93b6988f0eef85
matias <matiasbrighent5@gmail.com>
2025-02-28 22:36:03 -0300

    add "lixeira" e "correto"

--- main.js
+++ main.js
@@ -5,19 +5,30 @@ $(document).ready(function(){
         const novaTarefa = $('#tarefa').val();
         console.log(novaTarefa);
         const novoItem =$('<li style="display: none"></li>');
-        $(`<li> ${novaTarefa}</li>`).appendTo(novoItem);
+        $(
+            `<li> 
+                ${novaTarefa}
+                <img src="./imagens/correto.jpg" id="correto">
+                <img src="./imagens/lixxeira.jpg" id="lixeira">
+            </li>`
+        ).appendTo(novoItem);
+
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(700);
         $('#tarefa').val('');
         
-        $('ul').on('click','li', function(){
+        $('li').on('click', function(){
             $(this).css('text-decoration', 'line-through');
-            $(this).css('color', 'red');
+            $(this).css('color', 'green');
         })
 
-        $('ul').on('dblclick','li', function(){
+        $('li').on('dbclick', function(){
             $(this).css('text-decoration', 'none');
-                $(this).css('color', 'black')
+            $(this).css('color', 'black');
+        })
+
+        $('#lixeira').on('click',function(){
+            $('li').remove();
         })
 
         $('#limpar').on('click', function(){
